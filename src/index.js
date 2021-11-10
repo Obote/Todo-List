f8db1f63e873d6ee794058d98e6549c47098e97a
Obote <obotedenis256@gmail.com>
2021-11-10 22:13:39 +0300

    Add JS structure and function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const myToDo = document.getElementById('todos');
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'Obote'], ' ');
-  element.classList.add('hello');
+const todoList = [{
+  description: 'Feed pigs',
+  completed: false,
+  index: 1,
+}, {
+  description: 'Feed birds',
+  completed: false,
+  index: 2,
+}, {
+  description: 'Sweep the compound',
+  completed: false,
+  index: 3,
+},
+];
 
-  return element;
-}
-
-document.body.appendChild(component());
+const addContent = () => {
+  for (let i = 0; i < todoList.length; i + 1) {
+    const todoItem = document.createElement('li');
+    todoItem.innerHTML = `
+      <div>
+      <input type="checkbox" id="todo" name="todo">
+      <label for="todo">${todoList[i].description}</label>
+      <i class="fas fa-ellipsis-v"></i>
+      </div>
+      `;
+    todoItem.classList.add('todo');
+    myToDo.appendChild(todoItem);
+  }
+};
+addContent();
